e3df4628ea1908a2b29c008946f088ae3e41776d
Masudur Rahman <masudaucb1303@gmail.com>
2026-07-02 03:02:51 +0600

    Home footer added

---
+++ src/components/footer.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def footer_home():
+    st.markdown(f"""
+        <div style='margin:-1rem; display:flex; gap:6px; justify-content:center; item-align:center'>
+        
+        <p><b>Created With ❤️ by Masudur Rahman</b> </p>
+           
+        </div>      
+                """, unsafe_allow_html=True)
--- src/screen/home_screen.py
+++ src/screen/home_screen.py
@@ -2,11 +2,12 @@ import streamlit as st
 from src.components.header import header_home, subheader_home
 from src.ui.base_layout import style_backgroung_home, style_backgroung_dashboard, style_base_layout
 from src.components.column import column_teacher, column_student
+from src.components.footer import footer_home
 
 def home_screen():
     
     header_home()
-    subheader_home()
+    #subheader_home()
     style_backgroung_home()
     style_base_layout()
 
@@ -26,4 +27,18 @@ def home_screen():
         if st.button('Teacher Portal', type='primary'):
             st.session_state['login_type'] = 'teacher'
             st.rerun()
+
+    st.markdown(f"""
+                
+            <p>{"─"*62}</p>
+            
+                """, unsafe_allow_html=True)
+
+    footer_home()
+
+    st.markdown(f"""
+            
+        <p>{"─"*62}</p>
+        
+            """, unsafe_allow_html=True)
     
